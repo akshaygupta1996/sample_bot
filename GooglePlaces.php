02bbb8ae16c5e8699a15be8e29e9907b7e9bc892
akshaygupta1996 <gupta.akshay1996@gmail.com>
2016-09-29 23:45:19 +0530

    2.0.0

--- GooglePlaces.php
+++ GooglePlaces.php
@@ -1,15 +1,33 @@
 <?php
 
 
-
-
-function getPlaces($lat,$lng)
+class SavePlaces
 {
+	private $lat;
+	private $lng;
+	private $google_places_key;
+
+	function __construct($lat,$lng){
+
+		$this->lat=$lat;
+		$this->lng=$lng;
+		$this->google_places_key='AIzaSyD6qp5rZCwKBe5o1-BMmMwlCC194Fgz3xg';
+	}
+
+	function getNatPlaces($sender,$access_token)
+	{
+
+		$url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&radius=50000&type=natural_feature&key=$google_places_key";
+		$json=json_decode(file_get_contents($url,true));
+		$show=$json['results'][0]['geometry']['location']['lat'];
+		GiveReply($sender,"$show",$access_token);
 
 
+	}
 
-	
 }
+
+
 
 
 
